2deb8ca282c50119b2c574f4db148af5469eb5d7
Hassan A Hashim <h3ssan@protonmail.com>
2025-05-01 15:06:10 +0000

    Add static and media file configurations to settings and urls

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media files (user-uploaded files)
+# https://docs.djangoproject.com/en/5.2/topics/files/
+
+MEDIA_URL = "uploads/"
+MEDIA_ROOT = BASE_DIR / "uploads"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
