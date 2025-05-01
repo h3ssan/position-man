8d5ad15d0a2d84928d6e3479226bccbc1da51125
Hassan A Hashim <h3ssan@protonmail.com>
2025-05-01 13:25:45 +0000

    Add CSRF trusted origins configuration to settings

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,8 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
+
 
 # Application definition
 
